057b12975e28a437cf2c0d04e40fd512ee874ecd
ROHAN-88 <therohanshrestha6655@gmail.com>
2025-07-10 09:38:19 +0545

    updated

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(express.json());
 app.use(cors());
 
 /*//!ROUTES */
-
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
 app.use(userRoutes);
 app.use(postRoutes);
 app.use(savedRoutes);
